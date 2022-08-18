dbe62ba5747595c7630e8df3b033b8bbd69ccfec
Jamie Thomason <jamie.revill@gmail.com>
2022-08-18 17:55:03 -0400

    added apiRoutes and linked json to GitHub

---
+++ routes/apiRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const readFile = util.promisify(fs.readFile);
+const getNotes = () => {
+  return readFile('db/db.json', 'utf-8').then(rawNotes => {
+    let notesArray = [].concat(JSON.parse(rawNotes))
+    return notesArray
+  })
+};
+
+//GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/notes', async(req, res) => {
+  const result = await getNotes();
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+
+//POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+router.post('/notes', async(req, res) => {
+  const oldNotes = await getNotes()
+  let newNote = {title:req.body.title, text:req.body.text, id:uuidv4()}
+  const newArray = [...oldNotes,newNote]
+  console.log(newArray)
+  fs.writeFileSync('db/db.json', JSON.stringify(newArray));
+  res.json({msg:'ok'})
+});
+
+router.delete('/notes/:id', async(req,res) => {
+  console.log(req.params.id)
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
