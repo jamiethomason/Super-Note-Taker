4b3cf54de254411b3f008d60e744692594a517b2
Jamie Thomason <jamie.revill@gmail.com>
2022-08-22 17:03:04 -0400

    fixed pathway

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,6 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (req,res) => {res.sendFile(path.join(__dirname, '../public/notes.html'))});
-router.get('*', (req,res) => {res.sendFile(path.join(__dirname, '../public/index.html'))});
+router.get('*', (req,res) => {res.sendFile(path.join(__dirname, '../index.html'))});
 
-module.exports = router
+module.exports = router 
